ccf208509807a25e34d20c95beebee5b76cf5521
Tomasz Nowakowski <toalak@gmail.com>
2023-04-04 20:42:54 +0200

    Yellow book, chapter 3.1.9, out references

---
+++ yellow-book/3-1-9/3-1-9/OutReferences.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class OutReferences
+{
+    static string readString(string prompt)
+    {
+        string result;
+        do
+        {
+            Console.Write(prompt);
+            result = Console.ReadLine();
+        } while (result == "");
+        return result;
+    }
+
+    static int readInt(string prompt, int low, int high)
+    {
+        int result;
+        do
+        {
+            string intString = readString(prompt);
+            result = int.Parse(intString);
+        } while ((result < low) || (result > high));
+        return result;
+    }
+
+    static void readPerson(out string name, out int age)
+    {
+        name = readString("Enter your name: ");
+        age = readInt("Enter your age: ", 0, 100);
+    }
+
+    public static void Main()
+    {
+        string name;
+        int age;
+        readPerson(out name, out age);
+    }
+}
