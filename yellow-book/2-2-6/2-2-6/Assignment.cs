6ec606690689b07a4522274b9786c150ff950070
Tomasz Nowakowski <toalak@gmail.com>
2023-04-04 14:37:47 +0200

    The yellow book done up to chapter 2 incl.

---
+++ yellow-book/2-2-6/2-2-6/Assignment.cs
@@ -0,0 +1,10 @@
+﻿class Assignment
+{
+    static void Main()
+    {
+        int first, second, third;
+        first = 1;
+        second = 2;
+        third = second + first;
+    }
+}
---
+++ yellow-book/2-3-3-b/2-3-3-b/BreakContinue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class BreakContinue
+{
+    public static void Main()
+    {
+        int do_while_counter = 0;
+        Console.WriteLine("do - while loop starts...");
+        do
+        {
+            do_while_counter++;
+            if (do_while_counter % 2 == 0)
+            {
+                continue;
+            }
+            Console.WriteLine(do_while_counter);
+        }
+        while (do_while_counter < 10);
+        Console.WriteLine("do - while loop ends.");
+
+        Console.WriteLine();
+
+        Console.WriteLine("while loop starts...");
+        int while_counter = 0;
+        while (while_counter < 10)
+        {
+            while_counter++;
+            if (while_counter > 5)
+            {
+                break;
+            }
+            Console.WriteLine(while_counter);
+        }
+        Console.WriteLine("while loop ends.");
+    }
+}
