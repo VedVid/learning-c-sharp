7ad4aeebe7a155a8b56a02088665eb8a5f0f1a26
Tomasz Nowakowski <toalak@gmail.com>
2023-04-04 20:00:31 +0200

    Yellow book, chapter 3.1.2, silly method example with arguments; a little bit modified in comparison to the book version (additional silly2 method to check returning parameters).

---
+++ yellow-book/3-1-2/3-1-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MethodDemo
+{
+    static void silly(int i)
+    {
+        Console.WriteLine("i is: " + i);
+    }
+
+    static int silly2(int i)
+    {
+        return i;
+    }
+
+    public static void Main()
+    {
+        silly(101);
+        silly(500);
+
+        Console.WriteLine("i is: " + silly2(1));
+    }
+}
