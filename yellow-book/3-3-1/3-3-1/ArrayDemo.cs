7a25e23ca3efcb4701540b27f7033a9541096a9a
Tomasz Nowakowski <toalak@gmail.com>
2023-04-05 19:14:51 +0200

    Yellow book, chapter 3.3.1 with improvements from 3.3.2 and 3.3.3, array demo

---
+++ yellow-book/3-3-1/3-3-1/ArrayDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class ArrayDemo
+{
+    static string readString(string prompt)
+    {
+        string result;
+        do
+        {
+            Console.Write(prompt);
+            result = Console.ReadLine();
+        } while (result == "");
+        return result;
+    }
+
+    static int readInt(string prompt, int low, int high)
+    {
+        int result;
+        do
+        {
+            string intString = readString(prompt);
+            result = int.Parse(intString);
+        } while ((result < low) || (result > high));
+        return result;
+    }
+
+    public static void Main()
+    {
+        const int SCORE_SIZE = 11;
+        int[] scores = new int[SCORE_SIZE];
+        for (int i=0; i<SCORE_SIZE; i=i+1)
+        {
+            scores[i] = readInt("Score: ", 0, 1000);
+        }
+    }
+}
