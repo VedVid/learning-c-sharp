fb777eba43951f87a311bf32f13c09e8672577de
Tomasz Nowakowski <toalak@gmail.com>
2023-04-06 13:38:34 +0200

    Yellow book, chapter 4.5.3, account example

---
+++ yellow-book/4-5-3/4-5-3/AccountExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Account
+{
+    private decimal balance = 0;
+
+    public bool WithdrawFunds(decimal amount)
+    {
+        if (balance < amount)
+        {
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+
+    public void PayInFunds(decimal amount)
+    {
+        balance += amount;
+    }
+
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+}
+
+class AccountExample
+{
+    public static void Main()
+    {
+        Account account = new Account();
+        account.PayInFunds(50);
+        if (account.GetBalance() != 50)
+        {
+            Console.WriteLine("Pay In test failed");
+        }
+        else
+        {
+            Console.WriteLine("Pay In test succeeded");
+        }
+    }
+}
