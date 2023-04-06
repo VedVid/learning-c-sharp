9df3761429c026bf205d2568aea0cd9497576aea
Tomasz Nowakowski <toalak@gmail.com>
2023-04-06 21:22:51 +0200

    Working through the fourth chapter of the Yellow Book

---
+++ yellow-book/4-6-2/4-6-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Foo
+{
+    public static int bar = 0;
+    private static int baz = 5;
+    public int GetBaz()
+    {
+        return baz;
+    }
+}
+
+class StaticTest
+{
+    public static void Main()
+    {
+        Foo test = new Foo();
+        // Since 'bar' is static, writing 'test.bar' will not compile.
+        Console.WriteLine(Foo.bar);
+        // 'baz' is private so it can't be printed directly at all
+        // Console.WriteLine(Foo.baz); will not compile
+        // Public methods are called by the instance, and not a class.
+        Console.WriteLine(test.GetBaz());
+    }
+}
---
+++ yellow-book/4-6-3/4-6-3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Partial example, does not compile.
+
+public static bool AccountAllowed(decimal income, int age)
+{
+    if ((income >= 10000) && (age >= 18))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+/* This is interesting example. Since static methods are called by class
+   and not by instance, you can check if user is allowed to create an account
+   before even trying to create account.
+   If it were non-static method, you would need to create an instance of Account
+   first, and then check if user data fits the bill.
+*/
