270e472003da2bca6eb39be1e42a5e159eb1d0ec
Tomasz Nowakowski <toalak@gmail.com>
2023-04-07 21:10:11 +0200

    Yellow book, chapter 4.7.5, constructor

---
+++ yellow-book/4-7-5/4-7-5/Program.cs
@@ -0,0 +1,19 @@
+﻿class Account
+{
+    // Partial example, does not compile.
+    public Account(string inName, string inAddress, decimal inBalance)
+    {
+        string err = "";
+        if (SetBalance(inBalance) == false)
+            err = err + "Bad balance: " + inBalance;
+
+        if (SetName(inName) == false)
+            err = err + "Bad name: " + inName;
+
+        if (SetAddress(inAddress) == false)
+            err = err + "Bad addr: " + inAddress;
+
+        if (err != "")
+            throw new Exception("Account construction failed. " + err);
+    }
+}
