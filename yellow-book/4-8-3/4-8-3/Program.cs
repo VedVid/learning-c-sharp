e11e55e2bd9aa063270021b10b0b49a1dc0e9289
Tomasz Nowakowski <toalak@gmail.com>
2023-04-07 21:27:41 +0200

    Yellow book, chapter 4.8.3, interfaces

---
+++ yellow-book/4-8-3/4-8-3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+/* Looks like interafaces work a tad different that
+ * I thought.
+ * You _first_ declare interface, and then _later_ you implement
+ * class that fits the interface.
+*/
+
+public interface IAccount
+{
+    void PayInFunds(decimal amount);
+    
+    bool WithdrawFunds(decimal amount);
+
+    decimal GetBalance();
+}
+
+public class CustomerAccount: IAccount
+{
+    private decimal balance = 0;
+
+    public bool WithdrawFunds(decimal amount)
+    {
+        if (balance < amount)
+            return false;
+        balance -= amount;
+        return true;
+    }
+
+    public void PayInFunds(decimal amount)
+    {
+        balance += amount;
+    }
+
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+}
+
+class Bank
+{
+    public static void Main()
+    {
+        IAccount account = new CustomerAccount();
+        account.PayInFunds(50);
+        Console.WriteLine("Balance: " + account.GetBalance());
+    }
+}
